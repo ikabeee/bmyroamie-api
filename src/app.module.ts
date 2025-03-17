43f3d960e9e3fc1ff005a3f9e4fcebda73bfc0e0
SebasEPV <sperezvinas@gmail.com>
2025-03-17 18:57:25 -0500

    Added personality Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AdModule } from './ad/ad.module';
+import { PersonalityModule } from './personality/personality.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }), UserModule, PrismaModule, AdModule],
+  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }), UserModule, PrismaModule, AdModule, PersonalityModule],
   controllers: [],
   providers: [],
 })
