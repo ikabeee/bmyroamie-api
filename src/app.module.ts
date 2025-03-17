8e021947903a0deea19036471cf910f9c6986223
SebasEPV <sperezvinas@gmail.com>
2025-03-16 22:05:41 -0500

    Added prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }), UserModule],
+  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }), UserModule, PrismaModule],
   controllers: [],
   providers: [],
 })
