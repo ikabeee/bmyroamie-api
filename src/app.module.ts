6fec87afd738ae704b9b0dd8f20e7e2e726d8c7d
SebasEPV <sperezvinas@gmail.com>
2025-03-18 22:50:15 -0500

    Add Rule module with CRUD operations and integrate into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { InterestModule } from './interest/interest.module';
 import { StateModule } from './state/state.module';
 import { MunicipalityModule } from './municipality/municipality.module';
 import { AmenityModule } from './amenity/amenity.module';
+import { RuleModule } from './rule/rule.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }), UserModule, PrismaModule, AdModule, PersonalityModule, InterestModule, StateModule, MunicipalityModule, AmenityModule],
+  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }), UserModule, PrismaModule, AdModule, PersonalityModule, InterestModule, StateModule, MunicipalityModule, AmenityModule, RuleModule],
   controllers: [],
   providers: [],
 })
