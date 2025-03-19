47ae57b70d4c48d393029f3d7097392d12bc6da4
SebasEPV <sperezvinas@gmail.com>
2025-03-18 22:45:18 -0500

    Add Amenity module with CRUD operations and integrate into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { PersonalityModule } from './personality/personality.module';
 import { InterestModule } from './interest/interest.module';
 import { StateModule } from './state/state.module';
 import { MunicipalityModule } from './municipality/municipality.module';
+import { AmenityModule } from './amenity/amenity.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }), UserModule, PrismaModule, AdModule, PersonalityModule, InterestModule, StateModule, MunicipalityModule],
+  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }), UserModule, PrismaModule, AdModule, PersonalityModule, InterestModule, StateModule, MunicipalityModule, AmenityModule],
   controllers: [],
   providers: [],
 })
