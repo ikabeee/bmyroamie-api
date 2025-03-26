496771c6434bb10296ec4c600dd9b5dc9cbad315
ikabeee <carlglz30@gmail.com>
2025-03-25 22:45:05 -0500

    fix: Imports on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { StateModule } from './state/state.module';
+import { RuleModule } from './rule/rule.module';
+import { PersonalityModule } from './personality/personality.module';
+import { MunicipalityModule } from './municipality/municipality.module';
+import { InterestModule } from './interest/interest.module';
+import { ImageModule } from './image/image.module';
+import { FavoriteModule } from './favorite/favorite.module';
+import { AuthModule } from './auth/auth.module';
+import { AmenityModule } from './amenity/amenity.module';
+import { AdModule } from './ad/ad.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    UserModule,
+    StateModule,
+    RuleModule,
+    PersonalityModule,
+    MunicipalityModule,
+    InterestModule,
+    ImageModule,
+    FavoriteModule,
+    AuthModule,
+    AmenityModule,
+    AdModule,
+  ],
   controllers: [],
   providers: [],
 })
